8e8d882eb302e8af2ac355bc07be3ec2e717f288
Madhulika Dubey <57287028+madhulika3016@users.noreply.github.com>
2023-07-04 15:09:22 +0530

    commit

---
+++ Javarepo1/src/main/java/com/AwsCodeDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodeDeployDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodeDeployDemoApplication.class, args);
+	}
+
+}
---
+++ Javarepo1/src/main/java/com/HelloController.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+	
+	@GetMapping("/welcome")
+	public String welcome()
+	{
+		return "Code is deployed in Code Deploy Again";
+	}
+
+}
